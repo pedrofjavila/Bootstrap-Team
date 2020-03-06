62fc52ac0c6bc5063aba1a4f8052b9661fd47c85
julio <juliossilva91@gmail.com>
2020-03-06 19:07:55 -0100

    Added messages

--- server/src/main/java/org/academiadecodigo/apiores/bootstrap/Messages.java
+++ server/src/main/java/org/academiadecodigo/apiores/bootstrap/Messages.java
@@ -1,4 +1,16 @@
 package org.academiadecodigo.apiores.bootstrap;
 
 public class Messages {
+
+    public static final String WELCOME_MSG= "\t\t Welcome to the CADET QUIZ ";
+    public static final String WELCOME_RULES = " Players will join a quiz that have to answer a question from multiple available answers, only 1 answer is right.";
+    public static final String WELCOME_RULES1 = " Each player will have a time to select the correct answer, after that time expires players will move to the next question";
+    public static final String WELCOME_RULES2 = "The player with most answers right will win the quiz.";
+    public static final String PLAYER_IS_READY = "Are  you ready to start the quiz?";
+    public static final String PLAYER_READY  = " Cadet, brace yourself for impact";
+    public static final String GAME_JOIN_WAITING = " Waiting for all the players to join";
+    public static final String GAME_START = " START ";
+    public static final String QUESTION_WAITING = "Waiting for all the players to answer the question                         ";
+    public static final String QUESTION_RIGHT = "Your answer is right!";
+    public static final String QUESTION_WRONG = " You fail to answer";
 }
